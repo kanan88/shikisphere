4d2ad14fb03594ddc0c3ae9b392a73a9d87e2424
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-22 04:23:14 +0100

    implement initial code cleanup and initial setup

---
+++ components/Footer.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+      <p className="text-base font-bold text-white">
+        &copy; ShikiSphere {new Date().getFullYear()}
+      </p>
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={47}
+        height={44}
+        className="object-contain"
+      />
+      <div>
+        <Link
+          href="https://github.com/kanan88/shikisphere"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-6"
+        >
+          <Image
+            src="/github.svg"
+            alt="logo"
+            width={19}
+            height={19}
+            className="object-contain"
+          />
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
